35ca17ffe7cdbabdb00b2da3cac934ef3957767a
spatially021 <82856043+spatially021@users.noreply.github.com>
2024-09-11 09:48:34 +0900

    init & add task

---
+++ 04_example1/04_example1.ino
@@ -0,0 +1,16 @@
+#define PIN_LED 13
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(PIN_LED,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(PIN_LED,1);
+  
+
+}
